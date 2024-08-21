ba864003e9e6d96f77df08db78bbd99f5a6c44c0
Chakrala Sairam <csairam@IN-EDPS219-LT.enphaseenergy.com>
2024-08-21 10:53:59 +0530

    Added Junits for addEvse

--- src/test/java/com/sampleProject/evse/provision/service/EvseServiceTest.java
+++ src/test/java/com/sampleProject/evse/provision/service/EvseServiceTest.java
@@ -7,20 +7,20 @@ import com.sampleProject.evse.provision.repository.EvseRepo;
 import com.sampleProject.evse.provision.repository.SiteCustomRepo;
 import com.sampleProject.evse.provision.repository.SiteRepo;
 import com.sampleProject.evse.provision.requestDTO.EvseInitialInfoDto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.Mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import java.math.BigInteger;
 import java.time.Instant;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class EvseServiceTest {
@@ -42,6 +42,8 @@ public class EvseServiceTest {
 
     private BigInteger siteId = BigInteger.valueOf(12000000);
 
+    @Captor
+    ArgumentCaptor<Evse> evseArgumentCaptor;
 
 
 
@@ -50,21 +52,30 @@ public class EvseServiceTest {
         EvseInitialInfoDto evseInitialInfoDto = new EvseInitialInfoDto("123456789012","displayName_1");
         //Run the test
         evseServiceUnderTest.addEvse(siteId,evseInitialInfoDto);
-        Evse evse = new Evse();
+//        Evse evse = new Evse();
         String serialNum = evseInitialInfoDto.getSerialNumber();
         String displayName = evseInitialInfoDto.getDisplayName();
-        evse.setRetired(false);
-        evse.setSiteId(siteId);
-
-
-        evse.setDisplayName(displayName);
-        evse.setSerialNumber(serialNum);
-        evse.setEvseId(serialNum + ":" + siteId);
+//        evse.setRetired(false);
+//        evse.setSiteId(siteId);
+//
+//
+//        evse.setDisplayName(displayName);
+//        evse.setSerialNumber(serialNum);
+//        //mockito.anyString
+//        evse.setEvseId();
 
         verify(mockevseRepo, times(1)).findAllByIsRetiredAndSerialNumber(false,serialNum);
         verify(mocksiteCustomRepo, times(1)).increaseEvseCount(siteId);
-        verify(mockevseRepo, times(1)).insert(evse);
 
+        verify(mockevseRepo).insert(evseArgumentCaptor.capture());
+        Evse evseCaptorValue = evseArgumentCaptor.getValue();
+        Assertions.assertEquals(evseCaptorValue.getSiteId(),siteId);
+
+        Assertions.assertEquals(evseCaptorValue.getDisplayName(),displayName);
+        Assertions.assertEquals(evseCaptorValue.getSerialNumber(),serialNum);
+        Assertions.assertEquals(evseCaptorValue.isRetired(),false);
+        Assertions.assertEquals(evseCaptorValue.getEvseId().startsWith(serialNum), true);
+        Assertions.assertEquals(evseCaptorValue.getEvseId().endsWith(siteId.toString()),true);
     }
 
 }
