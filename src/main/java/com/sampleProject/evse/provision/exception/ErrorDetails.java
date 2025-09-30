5891516e8a2b3c09464daf92a0f4ec6a3c4de04b
Chakrala Sairam <csairam@IN-CSAIRAM-MBP.local>
2025-09-30 16:00:33 +0530

    to make it run

--- src/main/java/com/sampleProject/evse/provision/exception/ErrorDetails.java
+++ src/main/java/com/sampleProject/evse/provision/exception/ErrorDetails.java
@@ -12,7 +12,6 @@ import java.time.LocalDate;
 
 
 @Getter@Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class ErrorDetails {
     private LocalDate timeStamp;
@@ -25,6 +24,14 @@ public class ErrorDetails {
         this.timeStamp = now;
         this.status = httpStatus;
         this.message = message;
+        this.errorCode = errorCode;
+    }
+
+    public ErrorDetails(LocalDate now, HttpStatusCode httpStatusCode, String message, String path, int errorCode) {
+        this.timeStamp = now;
+        this.status = httpStatusCode;
+        this.message = message;
+        this.path = path;
         this.errorCode = errorCode;
     }
 }
